c06f52282a8536213d2171b7bf952776ef47271e
Adriancoding96 <118906436+Adriancoding96@users.noreply.github.com>
2024-04-17 14:57:02 +0100

    Fixed depth_traversal and added breadth_traversal logic

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <unordered_map>
 #include <vector>
+#include <queue>
 
-void depth_traversal(std::unordered_map<char, std::vector<char>> graph, char source = 'a');
-
+void depth_traversal(std::unordered_map<char, std::vector<char>> &graph, char source = 'a');
+void breadth_traversal(std::unordered_map<char, std::vector<char>> &graph, char source = 'a');
 
 
 int main() {
@@ -16,20 +17,23 @@ int main() {
             {'f', {}}
     };
 
+
     depth_traversal(graph, 'a');
+    //breadth_traversal(graph, 'a');
 
     return 0;
 }
 
+
 //Depth first traversal algorithm
 //Traverses graph deeper in a linear pattern, example of a -> b -> d then moves back to a -> c -> e -> b -> d
 //Uses a stack (Add to top and remove from top)
-void depth_traversal(std::unordered_map<char, std::vector<char>> graph, char source) {
+void depth_traversal(std::unordered_map<char, std::vector<char>> &graph, char source) {
     std::vector<char> stack {source}; //initialize stack with source node
 
     while(stack.size() > 0) { //Run as long as stack is not empty
-        char current = stack.front();
-        stack.erase(stack.begin());
+        char current = stack.back();
+        stack.pop_back();
         std::cout << current <<std::endl;
         for(char neighbour: graph[current]) { //Enters vector using current as key
             stack.push_back(neighbour); //Push neighbours to top of stack
@@ -42,3 +46,18 @@ void depth_traversal(std::unordered_map<char, std::vector<char>> graph, char sou
 //Breadth first traversal
 //Traverses graph by exploring all neighbours adjacent to the starting node
 //uses a que (Add from the back and remove from the front)
+void breadth_traversal(std::unordered_map<char, std::vector<char>> &graph, char source) {
+    std::queue<char> queue;
+    queue.push(source); //Push source on to queue
+
+    while(queue.size() > 0) {
+        char current = queue.front();
+        queue.pop();
+
+        std::cout << current << std::endl;
+        for(char neighbour: graph[current]) {
+            queue.push(neighbour); //push neighbors to back of queue
+        }
+    }
+}
+
